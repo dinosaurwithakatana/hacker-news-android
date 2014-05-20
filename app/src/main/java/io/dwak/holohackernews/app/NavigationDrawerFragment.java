04f0d8751f038057a067efb05e048da9ec6e5498
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2014-05-19 20:38:05 -0500

    updating launcher icon

--- app/src/main/java/io/dwak/holohackernews/app/NavigationDrawerFragment.java
+++ app/src/main/java/io/dwak/holohackernews/app/NavigationDrawerFragment.java
@@ -138,7 +138,7 @@ public class NavigationDrawerFragment extends Fragment {
         mDrawerToggle = new ActionBarDrawerToggle(
                 getActivity(),                    /* host Activity */
                 mDrawerLayout,                    /* DrawerLayout object */
-                R.drawable.ic_drawer,             /* nav drawer image to replace 'Up' caret */
+                R.drawable.ic_navigation_drawer,             /* nav drawer image to replace 'Up' caret */
                 R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
                 R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
         ) {
