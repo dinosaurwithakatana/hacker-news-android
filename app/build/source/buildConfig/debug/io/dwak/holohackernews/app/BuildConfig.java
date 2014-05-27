dd07f246e12631e4343ff41753546350b5f9d488
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2014-05-26 22:11:18 -0500

    updating to roboto light

---
+++ app/build/source/buildConfig/debug/io/dwak/holohackernews/app/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.dwak.holohackernews.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "io.dwak.holohackernews.app.dev";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-dev";
+  // Fields from build type: debug
+  public static final String READABILITY_TOKEN = "aa0c24f21c71917c2bde5835c5260e9e286e9a49";
+  public static final boolean TRAVIS = false;
+}
---
+++ app/build/source/buildConfig/travis/io/dwak/holohackernews/app/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.dwak.holohackernews.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "io.dwak.holohackernews.app.travis";
+  public static final String BUILD_TYPE = "travis";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-travis";
+  // Fields from build type: travis
+  public static final String READABILITY_TOKEN = null;
+  public static final boolean TRAVIS = true;
+}
