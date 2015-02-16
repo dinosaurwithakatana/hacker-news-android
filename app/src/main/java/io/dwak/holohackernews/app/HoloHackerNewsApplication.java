d8f390e536cf4af659af64b9d0ba5c46f367c43d
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-02-15 18:25:26 -0600

    Increase code readability

--- app/src/main/java/io/dwak/holohackernews/app/HoloHackerNewsApplication.java
+++ app/src/main/java/io/dwak/holohackernews/app/HoloHackerNewsApplication.java
@@ -7,14 +7,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.okhttp.OkHttpClient;
 
-import java.io.File;
-
 import io.dwak.holohackernews.app.manager.hackernews.LongTypeAdapter;
 import io.dwak.holohackernews.app.network.HackerNewsService;
-import io.dwak.holohackernews.app.network.LoginService;
 import io.dwak.holohackernews.app.preferences.LocalDataManager;
 import retrofit.RestAdapter;
-import retrofit.android.AndroidLog;
 import retrofit.client.OkClient;
 import retrofit.converter.GsonConverter;
 
@@ -26,7 +22,6 @@ public class HoloHackerNewsApplication extends Application {
     private static HoloHackerNewsApplication sInstance;
     private Context mContext;
     private HackerNewsService mHackerNewsService;
-    private LoginService mLoginService;
 
     @Override
     public void onCreate() {
@@ -51,10 +46,6 @@ public class HoloHackerNewsApplication extends Application {
         return mContext;
     }
 
-    public File getApplicationCacheDir(){
-        return getCacheDir();
-    }
-
     public HackerNewsService getHackerNewsServiceInstance(){
         if(mHackerNewsService==null) {
             GsonBuilder gsonBuilder = new GsonBuilder();
@@ -72,22 +63,4 @@ public class HoloHackerNewsApplication extends Application {
         return mHackerNewsService;
     }
 
-    public LoginService getLoginServiceInstance(){
-        if(mLoginService==null) {
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            gsonBuilder.registerTypeAdapter(Long.class, new LongTypeAdapter());
-            Gson gson = gsonBuilder.create();
-            RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setLogLevel(RestAdapter.LogLevel.FULL)
-                    .setLog(new AndroidLog("Retrofit"))
-                    .setClient(new OkClient(new OkHttpClient().setFollowSslRedirects(true)))
-                    .setConverter(new GsonConverter(gson))
-                    .setEndpoint("https://news.ycombinator.com")
-                    .build();
-
-            mLoginService = restAdapter.create(LoginService.class);
-        }
-
-        return mLoginService;
-    }
 }
--- app/src/main/java/io/dwak/holohackernews/app/ui/login/LoginActivity.java
+++ app/src/main/java/io/dwak/holohackernews/app/ui/login/LoginActivity.java
@@ -11,14 +11,10 @@ import com.dd.CircularProgressButton;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import io.dwak.holohackernews.app.HoloHackerNewsApplication;
 import io.dwak.holohackernews.app.R;
 import io.dwak.holohackernews.app.preferences.LocalDataManager;
-import retrofit.client.Header;
 import rx.Observable;
 import rx.android.observables.ViewObservable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -28,12 +24,14 @@ public class LoginActivity extends ActionBarActivity {
     @InjectView(R.id.username) EditText mUsername;
     @InjectView(R.id.password) EditText mPassword;
     @InjectView(R.id.login_button_with_progress) CircularProgressButton mLoginButton;
+    private LoginViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.inject(this);
+        mViewModel = new LoginViewModel();
 
         // Creates Observables from the EditTexts and enables the login button if they aren't empty
         final Observable<Boolean> userNameObservable = ViewObservable.text(mUsername, true)
@@ -41,33 +39,17 @@ public class LoginActivity extends ActionBarActivity {
         final Observable<Boolean> passwordObservable = ViewObservable.text(mPassword, true)
                 .map(editText -> !TextUtils.isEmpty(editText.getText()));
         Observable.combineLatest(userNameObservable, passwordObservable,
-                (aBoolean, aBoolean2) -> aBoolean && aBoolean2)
-                .subscribe(t1 -> {
+                (usernameFilled, passwordFilled) -> usernameFilled && passwordFilled)
+                .subscribe(fieldsFilled -> {
                     mLoginButton.setProgress(0);
-                    mLoginButton.setEnabled(t1);
+                    mLoginButton.setEnabled(fieldsFilled);
                 });
 
         ViewObservable.clicks(mLoginButton, false)
                 .subscribe(button -> {
                     mLoginButton.setIndeterminateProgressMode(true);
                     mLoginButton.setProgress(50);
-                    HoloHackerNewsApplication.getInstance()
-                            .getLoginServiceInstance()
-                            .login("news", mUsername.getText().toString(), mPassword.getText().toString())
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .map(response -> {
-                                String userCookie = null;
-                                for (Header header : response.getHeaders()) {
-                                    if ("set-cookie".equals(header.getName())) {
-                                        if (header.getValue().contains("user")) {
-                                            userCookie = header.getValue().substring(5);
-                                            userCookie = userCookie.split(";")[0];
-                                        }
-                                    }
-                                }
-                                return userCookie;
-                            })
+                    mViewModel.login(mUsername.getText().toString(), mPassword.getText().toString())
                             .subscribe(userCookie -> {
                                 if (userCookie == null) {
                                     mLoginButton.setProgress(-1);
@@ -76,8 +58,7 @@ public class LoginActivity extends ActionBarActivity {
                                     mLoginButton.setProgress(100);
                                     LocalDataManager.getInstance().setUserLoginCookie(userCookie);
                                     LocalDataManager.getInstance().setUserName(mUsername.getText().toString());
-                                    Intent loginIntent = new Intent(LOGIN_SUCCESS);
-                                    LocalBroadcastManager.getInstance(LoginActivity.this).sendBroadcast(loginIntent);
+                                    LocalBroadcastManager.getInstance(LoginActivity.this).sendBroadcast(new Intent(LOGIN_SUCCESS));
                                     LoginActivity.this.finish();
                                 }
                             });
