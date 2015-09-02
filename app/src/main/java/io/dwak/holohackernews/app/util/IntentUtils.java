5091385730f7dfb8c553ba7d648e88b1d47e44ae
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-09-01 20:51:46 -0500

    update share in intentutils

--- app/src/main/java/io/dwak/holohackernews/app/util/IntentUtils.java
+++ app/src/main/java/io/dwak/holohackernews/app/util/IntentUtils.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 
 public final class IntentUtils {
+    /**
+     * Opens the given url in the system browser
+     * @param context Context with which to open the browser from
+     * @param uri Url to open in the browser
+     * @see Intent#ACTION_VIEW
+     */
     public static void openInBrowser(final Context context, final String uri){
         Intent browserIntent = new Intent();
         browserIntent.setAction(Intent.ACTION_VIEW);
@@ -12,14 +18,16 @@ public final class IntentUtils {
         context.startActivity(browserIntent);
     }
 
+    /**
+     * Share plain text using system share
+     * @param context Context to share from
+     * @param text plain text to share
+     * @see Intent#ACTION_SEND
+     */
     public static void share(final Context context, final String text){
-        share(context, text, "text/plain");
-    }
-
-    public static void share(final Context context, final String text, final String type){
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
-        shareIntent.setType(type);
+        shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, text);
         context.startActivity(shareIntent);
     }
